bbafc5fb805606d9bd1af4917f2b916cd6cfafd6
naresargsyan1738 <nare.sargsyan@student.rau.am>
2025-12-31 04:26:22 +0400

    Get post put delete

--- app/routers/rock_groups.py
+++ app/routers/rock_groups.py
@@ -23,3 +23,38 @@ def add_group(group: RockGroupCreate):
     db.refresh(new_group)
     db.close()
     return new_group
+@router.get("/rock-groups/{group_id}")
+def get_group(group_id: int):
+    db = SessionLocal()
+    group = db.query(models.RockGroup).filter(models.RockGroup.id == group_id).first()
+    db.close()
+    return group
+
+@router.delete("/rock-groups/{group_id}")
+def delete_group(group_id: int):
+    db = SessionLocal()
+    group = db.query(models.RockGroup).filter(models.RockGroup.id == group_id).first()
+
+    if group:
+        db.delete(group)
+        db.commit()
+        db.close()
+        return {"status": "deleted"}
+
+    db.close()
+    return {"status": "not found"}
+@router.put("/rock-groups/{group_id}")
+def update_group(group_id: int, new_data: RockGroupCreate):
+    db = SessionLocal()
+    group = db.query(models.RockGroup).filter(models.RockGroup.id == group_id).first()
+
+    if group:
+        group.name = new_data.name
+        group.country = new_data.country
+        db.commit()
+        db.refresh(group)
+        db.close()
+        return group
+
+    db.close()
+    return {"status": "not found"}
